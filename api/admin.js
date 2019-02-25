103ab58d0661f7ade18d8aea8cd9f2517e5be57f
Vladislav_747 <zippo585@gmail.com>
2019-02-25 16:19:48 +0500

    Making of admin section

--- api/admin.js
+++ api/admin.js
@@ -1,5 +1,30 @@
 //Объект роутинга
 const express = require('express');
+const app = express();
 const router = express.Router();
 
+//Template operating
+//handlebars
+// set up handlebars view engine
+var handlebars = require('express3-handlebars').create({
+  defaultLayout: 'admin',
+  helpers: {
+    section: function (name, options) {
+      if (!this._sections) this._sections = {};
+      this._sections[name] = options.fn(this);
+      return null;
+    }
+  }
+});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+//Если запрос уходит методом GET
+app.get('/', (req, res) => {
+  console.log("Get запрос index");
+  res.render('home');
+});
+
+module.exports = app;
 
